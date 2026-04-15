735b52abab26c01dd31f5029f76cc8cdba48726b
AlexTran0616 <tranalex132@gmail.com>
2026-04-15 18:13:59 -0400

    Update SnakeGame.py
    - Added fruit spawning
    - Added eating fruit

--- SnakeGame.py
+++ SnakeGame.py
@@ -4,31 +4,46 @@ import random
 game_width = 25
 game_height = 20
 
-def fruit():
+def newFruitPosition():
     fruitPosition = (0, 0)
     
-    fruitPosition_Y = random.randint(0,24)
-    fruitPosition_X = random.randint(0,24)
+    fruitPosition_Y = random.randint(0,game_height-1)
+    fruitPosition_X = random.randint(0,game_width-1)
 
     fruitPosition = (fruitPosition_Y, fruitPosition_X)
     return fruitPosition
 
 def main(stdscr):
     curses.curs_set(0)
-    stdscr.timeout(100)
+    stdscr.timeout(150)
 
     position = [game_height //2, game_width //2]
+    fruitPosition = [game_height //2, game_width //2 + 5]
     direction = (0,1)
     gameOn = True
+    isFruit = False
 
     while(gameOn):
         stdscr.clear()
+
         for y in range(game_height):
             for x in range(game_width):
                 if [y, x] == position:
                     stdscr.addstr(y, x*3, "[O]")
                 else:
                     stdscr.addstr(y, x*3, "[ ]")
+                if position == fruitPosition:
+                    isFruit = False
+                    stdscr.addstr(fruitPosition[0], fruitPosition[1]*3, "[ ]")
+                    
+        if isFruit:
+            stdscr.addstr(fruitPosition[0], fruitPosition[1]*3, "[X]")
+        else:
+            fruitPosition[:] = newFruitPosition()
+            stdscr.addstr(fruitPosition[0], fruitPosition[1]*3, "[X]")
+            isFruit = True
+
+                    
         stdscr.refresh()
 
         key = stdscr.getch()
@@ -50,4 +65,3 @@ def main(stdscr):
         position[1] = max(0, min(game_width - 1, position[1]))
 
 curses.wrapper(main)
-fruit()
